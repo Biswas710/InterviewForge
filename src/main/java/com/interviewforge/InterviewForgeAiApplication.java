cf054363150f127c35410b930952c09d2b171655
Biswas710 <biswasthapa1035@gmail.com>
2026-07-03 02:25:32 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/interviewforge/InterviewForgeAiApplication.java
@@ -0,0 +1,13 @@
+package com.interviewforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewForgeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewForgeAiApplication.class, args);
+	}
+
+}
